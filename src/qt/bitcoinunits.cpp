46105e795736fbfb0d86271f41f1dca7ed347057
Derek M. Anderson <derek.anderson@coxinc.com>
2014-01-07 23:18:12 -0600

    updated some of the other litecoin branding
    NYN chosen as units

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("NYN");
+    case mBTC: return QString("mNYN");
+    case uBTC: return QString::fromUtf8("μNYN");
     default: return QString("???");
     }
 }
